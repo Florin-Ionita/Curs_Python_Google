0c2662f696e435a6996c8efb40d3b7838df8f4c7
Ionita-Florin <florinteodor.ionita@gmail.com>
2023-04-22 13:14:37 +0300

    sc

--- pythonProject/script.py
+++ pythonProject/script.py
@@ -1,12 +1,22 @@
-class Coordinate(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Animal(object):
+    def __init__(self, age, name):
+        self.age = age
+        self.name = None
+
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+    def set_name(self, name=""):
+        self.name = name
+
+    def set_age(self, new_age):
+        self.age = new_age
 
     def __str__(self):
-        return f"<{self.x}, {self.y}>"
+        return f"animal: {self.name}, varsta {self.age}"
 
-
-
-a = Coordinate(1,2)
+a = Coordinate(1, 2)
 print(a)
