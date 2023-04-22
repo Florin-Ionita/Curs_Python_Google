787ad4134fd17477d62c7c453c3ef3e7fb372004
Ionita-Florin <florinteodor.ionita@gmail.com>
2023-04-22 13:03:05 +0300

    c

--- pythonProject/script.py
+++ pythonProject/script.py
@@ -1,1 +1,12 @@
-print("Salut")
+class Coordinate(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"<{self.x}, {self.y}>"
+
+
+
+a = Coordinate(1,2)
+print(a)
